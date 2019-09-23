117dc8c21f65a9087c07d702486bfb012007809e
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2019-09-23 14:56:32 +0900

    Layout

---
+++ app/src/main/java/com/example/androidclass_0923/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidclass_0923;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.LinearLayout);
+    }
+}

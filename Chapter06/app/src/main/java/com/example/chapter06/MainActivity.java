8f4eaf9f5ba8705685168f3b59bb562104790e7a
YangHyunSeung <41092020+zmfoq12345@users.noreply.github.com>
2019-09-30 15:11:17 +0900

    TabHost_1

--- Chapter06/app/src/main/java/com/example/chapter06/MainActivity.java
+++ Chapter06/app/src/main/java/com/example/chapter06/MainActivity.java
@@ -2,13 +2,72 @@ package com.example.chapter06;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+//viewFlipper
+
+    Button btn1, btn2;
+    ViewFlipper vf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_slidingdrawer);
+        setContentView(R.layout.activity_viewflipper);
+
+        btn1 = (Button)findViewById(R.id.b1);
+        btn2 = (Button)findViewById(R.id.b2);
+        vf = (ViewFlipper)findViewById(R.id.ViewFlipper);
+
+//      <-버튼 정의
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                vf.setFlipInterval(250);
+//                vf.startFlipping();
+                vf.showPrevious();
+            }
+        });
+
+//      ->버튼 정의
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vf.showNext();
+//                vf.stopFlipping();
+            }
+        });
+
+
     }
 }
+
+
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_slidingdrawer);
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
